d9ef450e23ed9d948c576977cdf0cf146b8e0bc8
ethioendu <ethioendu21@gmail.com>
2023-03-06 12:06:19 -0800

    com

--- 0x00-python-hello_world/lists.h
+++ 0x00-python-hello_world/lists.h
@@ -1,20 +1,20 @@
 #ifndef LISTS_H
 #define LISTS_H
-
 #include <stdlib.h>
+
 /**
  * struct listint_s - singly linked list
  * @n: integer
- * @next: point to the next node
+ * @next: points to the next node
  *
- * Description: single linked list node structure 
- *
+ * Description: singly linked list node structure
+ * 
  */
 typedef struct listint_s
 {
-	int n;
-	struct listint_s *next;
-}listint_t;
+    int n;
+    struct listint_s *next;
+} listint_t;
 
 size_t print_listint(const listint_t *h);
 listint_t *add_nodeint(listint_t **head, const int n);
