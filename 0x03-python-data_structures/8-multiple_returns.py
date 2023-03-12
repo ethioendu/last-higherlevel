614f27cb956ddb3ea19280dc625f1cbb2412d76b
ethioendu <ethioendu21@gmail.com>
2023-03-12 12:48:50 -0700

    project

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,3 +1,6 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-        return len(sentence), sentence[0] if sentence else None
+    if not sentence:
+        return (0, None)
+    else:
+        return len(sentence), sentence[0] 
