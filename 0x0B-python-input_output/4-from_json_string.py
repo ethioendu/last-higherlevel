8bd644000ce74ee0b02c2ef023666398390db0cf
ethioendu <ethioendu@github.com>
2023-07-12 14:54:34 -0700

    4-from_json_string.py

--- 0x0B-python-input_output/4-from_json_string.py
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 """
-contains the json str function 
+contains the json str function
 """
 
 import json
+
 
 def from_json_string(my_str):
     """returns an object represented by a JSON string"""
