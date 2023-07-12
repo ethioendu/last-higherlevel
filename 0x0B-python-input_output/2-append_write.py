1b6227e342cc67033b13d2c4590bba060517bb65
ethioendu <ethioendu@github.com>
2023-07-12 14:15:42 -0700

    2-append_write.py

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -1,7 +1,11 @@
 #!/usr/bin/python3
 """
 contains the function "write_file"
+
 """
+Contains the function "wrtie_file"
+"""
+
 
 def write_file(filename="", text=""):
     """returns the number of chars written to "filename" from "text" """
