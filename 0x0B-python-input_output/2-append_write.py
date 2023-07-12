b3c222817946b2c8cc4bcbe000daecbd7510a525
ethioendu <ethioendu@github.com>
2023-07-12 14:07:16 -0700

    2-append_write.py

---
+++ 0x0B-python-input_output/2-append_write.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""
+contains the function "write_files"
+"""
+
+def write_file(filename="", text=""):
+    """returns the number of chars written to filename" from "text" """
+    with open(filename, 'w', encoding='utf=8') as f:
+        return f.write(text)
